10af27d0c1234d397d42cd9bbf5939a5833acf4e
Raí Joia <megac154@gmail.com>
2025-10-08 21:56:36 -0300

    feat: initial project

---
+++ backend/src/main/java/br/com/alertabank/alertabank/AlertabankApplication.java
@@ -0,0 +1,13 @@
+package br.com.alertabank.alertabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertabankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/alertabank/alertabank/AlertabankApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.alertabank.alertabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertabankApplicationTests {
+
+}

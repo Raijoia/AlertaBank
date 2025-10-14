6d99e5767c8b5cb2a6e91a33d0634af8ce4fdd87
Raí Joia <megac154@gmail.com>
2025-10-14 20:59:50 -0300

    feat: add models

---
+++ backend/src/main/java/br/com/alertabank/alertabank/model/Banco.java
@@ -0,0 +1,27 @@
+package br.com.alertabank.alertabank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_bancos")
+@Getter
+@Setter
+public class Banco {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+}
---
+++ backend/src/main/java/br/com/alertabank/alertabank/model/Relato.java
@@ -0,0 +1,37 @@
+package br.com.alertabank.alertabank.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_relatos")
+@Getter
+@Setter
+public class Relato {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "banco_id", nullable = false)
+    private Banco banco;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+
+    @CreationTimestamp
+    @Column(name = "data_ocorrido", updatable = false)
+    private LocalDateTime dataOcorrido;
+}
